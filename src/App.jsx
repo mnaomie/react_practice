4fc89b8308d3239f2da1934d43499eeb4db790ad
naomie marco <naomiemarco7@gmail.com>
2025-01-06 14:23:50 +0100

    API handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { User } from './Avatar'
+import axios from 'axios'
 
 
 function App() {
 
-  const characters = [
-  {
-    firstName: 'Bart',
-    lastName: 'Simpson',
-    image: 'https://static.wikia.nocookie.net/heroes-fr/images/3/3b/Bart_Simpson_.png/revision/latest?cb=20200819150449&path-prefix=fr' 
-  },
-  {
-    firstName: 'Homer',
-    lastName: 'Simpson',
-    image: 'https://static.wikia.nocookie.net/simpsons/images/3/33/Homer.png/revision/latest?cb=20110703093455&path-prefix=fr'
+  const [users, setUsers] = useState(null);
+  const URL = 'https://jsonplaceholder.typicode.com/users';
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(URL)
+      setUsers(response.data)
+    } catch (err) {
+      console.error(err);
+      
+    }
+
+    useEffect(() => {
+      fetchUsers()
+    }, [])
   }
-]
   return (
     <>
       <div>
-        <h1>Simpson Family</h1>
-        {characters.map(character => {
+        <h1>Users List</h1>
+        {users && users.map(user => {
           return (
-            <User {...character}/>
+            <>
+              <h3>My name is {user.name}</h3>
+              <p>Email : {user.email}</p>
+            </>
           )
         })}
       </div>
     </>
   )
+  
 }
 
 export default App;
